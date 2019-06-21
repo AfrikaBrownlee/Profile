90f0615bf7759099881af4c9fd26674fe5c15668
afrikabrownlee <afrikabrownlee@gmail.com>
2019-06-21 13:30:20 -0400

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-  console.log(`Example app listening on port 5000!`);
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.use(express.static(__dirname + '/Public'));
